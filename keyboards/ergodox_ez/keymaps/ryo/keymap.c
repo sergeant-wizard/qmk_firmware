202c26b9e79dee4cbfa565e17ce5e4ffcb5ccf2d
Ryo Miyajima <ryo@preferred.jp>
2018-02-11 20:13:59 +0900

    right brace to the left

--- keyboards/ergodox_ez/keymaps/ryo/keymap.c
+++ keyboards/ergodox_ez/keymaps/ryo/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |      | Ins  | Win  |  Alt | Prnt |                                       |  Up  | Down |   =  |   ]  |   \  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |  ]   |       |  \   |   =  |
+ *                                        |   ]  |      |       |  \   |   =  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | BS   | ESC  |------|       |------|  Enter |Space |
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTRL,       KC_A,         KC_S,   KC_D,      KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,      KC_V,   KC_B,   ALL_T(KC_NO),
         MO(SYMB),       KC_INSERT,    KC_LGUI,KC_LALT,   KC_PSCR,
-                                                    KC_TRNS, KC_RBRC,
+                                                     KC_RBRC, KC_TRNS,
                                                               KC_HOME,
                                                KC_BSPC,KC_ESC,KC_END,
         // right hand
