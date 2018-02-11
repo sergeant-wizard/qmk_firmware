4ab58bf070fca7511bf28356294e48400ab3b2a1
Ryo Miyajima <ryo@preferred.jp>
2018-02-12 06:08:47 +0900

    all symbols closer

--- keyboards/ergodox_ez/keymaps/ryo/keymap.c
+++ keyboards/ergodox_ez/keymaps/ryo/keymap.c
@@ -20,16 +20,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   [    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  \   |   Y  |   U  |   I  |   O  |   P  |   [    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | ' / L2 |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * |--------+------+------+------+------+------|  ]   |           |  =   |------+------+------+------+------+--------|
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      | Ins  | Win  |  Alt | Prnt |                                       |  Up  | Down |   =  |   ]  |   \  |
+ *   |  L1  | Ins  | Win  |  Alt | Prnt |                                       |  Up  | Down |   =  |   ]  |   \  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |   ]  |      |       |  \   |   =  |
+ *                                        |Hyper |      |       |      | Meh  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | BS   | ESC  |------|       |------|  Enter |Space |
@@ -43,18 +43,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_1,         KC_2,   KC_3,      KC_4,   KC_5,   KC_LEFT,
         KC_TAB,         KC_Q,         KC_W,   KC_E,      KC_R,   KC_T,   TG(SYMB),
         KC_LCTRL,       KC_A,         KC_S,   KC_D,      KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,      KC_V,   KC_B,   ALL_T(KC_NO),
-        MO(SYMB),       KC_INSERT,    KC_LGUI,KC_LALT,   KC_PSCR,
-                                                     KC_RBRC, KC_TRNS,
+        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,      KC_V,   KC_B,   KC_RBRC,
+        MO(SYMB),       KC_INS,       KC_LGUI,KC_LALT,   KC_PSCR,
+                                                 ALL_T(KC_NO),KC_TRNS,
                                                               KC_HOME,
                                                KC_BSPC,KC_ESC,KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_LBRC,
-                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          LT(MDIA, KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP, KC_DOWN,KC_EQL, KC_RBRC,          KC_BSLASH,
-             KC_BSLASH,      KC_EQL,
+             KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,    KC_MINS,
+             KC_BSLS,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,    KC_LBRC,
+                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN, LT(MDIA, KC_QUOT),
+             KC_EQL,      KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
+                                  KC_UP, KC_DOWN,KC_EQL, KC_RBRC, KC_BSLS,
+             KC_TRNS,        MEH_T(KC_NO),
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
     ),
