cccb968410610d5891b6a6f28ce22768530dc2d2
Ryo Miyajima <ryo@preferred.jp>
2018-02-11 18:48:49 +0900

    fix trans typo

--- keyboards/ergodox_ez/keymaps/ryo/keymap.c
+++ keyboards/ergodox_ez/keymaps/ryo/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,      KC_R,   KC_T,   TG(SYMB),
         KC_LCTRL,       KC_A,         KC_S,   KC_D,      KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,      KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_TRANS,       KC_INSERT,    KC_LGUI,KC_LALT,   KC_PSCR,
+        KC_TRNS,        KC_INSERT,    KC_LGUI,KC_LALT,   KC_PSCR,
                                                     KC_TRNS, KC_RBRC,
                                                               KC_HOME,
                                                KC_BSPC,KC_ESC,KC_END,
