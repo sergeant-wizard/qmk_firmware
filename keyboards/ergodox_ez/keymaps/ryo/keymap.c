a6bd94c028e5ae01b897ff517baa19394359de44
Ryo Miyajima <ryo@preferred.jp>
2018-02-13 07:22:05 +0900

    move L2 switch from hyphen to bottom-right

--- keyboards/ergodox_ez/keymaps/ryo/keymap.c
+++ keyboards/ergodox_ez/keymaps/ryo/keymap.c
@@ -22,11 +22,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  \   |   Y  |   U  |   I  |   O  |   P  |   [    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | ' / L2 |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|  ]   |           |  =   |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  | Ins  | Win  |  Alt | Prnt |                                       |  Up  | Down |   =  |   ]  |   \  |
+ *   |  L1  | Ins  | Win  |  Alt | Prnt |                                       |  Up  | Down |   =  |   ]  |  L2  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |Hyper |      |       |      | Meh  |
@@ -51,9 +51,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,    KC_MINS,
              KC_BSLS,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,    KC_LBRC,
-                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN, LT(MDIA, KC_QUOT),
+                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN, KC_QUOT,
              KC_EQL,      KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
-                                  KC_UP, KC_DOWN,KC_EQL, KC_RBRC, KC_BSLS,
+                                  KC_UP, KC_DOWN,KC_EQL, KC_RBRC, MO(MDIA),
              KC_TRNS,        MEH_T(KC_NO),
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
